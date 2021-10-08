dc77db9545bc1a63452a15b2a767014dd1e69711
weely <lm220930@163.com>
2021-10-08 17:03:48 +0800

    refector: json工具改写成setup组件

--- src/components/JsonEditor/index.vue
+++ src/components/JsonEditor/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="json-editor">
-    <textarea ref="textarea" />
+    <textarea ref="textarea"></textarea>
   </div>
 </template>
 
@@ -17,75 +17,89 @@ import 'codemirror/addon/fold/foldgutter.css'
 import 'codemirror/addon/fold/foldcode.js'
 import 'codemirror/addon/fold/brace-fold.js'
 import 'codemirror/addon/fold/brace-fold.js'
+import { ref, toRefs, reactive, watch, nextTick, onMounted } from 'vue'
 
 export default {
   name: 'JsonEditor',
   props: ['value'],
-  data() {
-    return {
-      jsonEditor: false,
-    }
-  },
-  watch: {
-    value(value) {
-      const editorValue = this.jsonEditor.getValue()
-      if (value !== editorValue) {
-        this.jsonEditor.setValue(JSON.stringify(this.value, null, 2))
-      }
-    },
-  },
-  mounted() {
-    this.jsonEditor = CodeMirror.fromTextArea(this.$refs.textarea, {
-      theme: 'rubyblue',
-      mode: 'application/json',
-      lint: true,
-      lineNumbers: true,  // 显示行号
-      lineWrapping: true, // 文字过长时，换行
-      // showCursorWhenSelecting: true,  // 文本选中时显示光标
-      // cursorHeight: 0.85, //光标高度，默认是1
-      // matchBrackets: true, // 括号匹配
-      // smartIndent: true, // 智能缩进
-      foldGutter: true,
-      gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter', 'CodeMirror-lint-markers'],
-      extraKeys: {'Ctrl-Q': function(cm){ cm.foldCode(cm.getCursor()) }},
-      foldOptions: {
-        widget: (from, to) => {
-          var count = undefined
+  emits: ['update:value'],
+  setup(props, context) {
+    const { value } = toRefs(props)
+    const textarea = ref(null)
+    let jsonEditor = reactive(null)
 
-          // Get open / close token
-          var startToken = '{', endToken = '}'
-          var prevLine = this.jsonEditor.getLine(from.line)
-          if (prevLine.lastIndexOf('[') > prevLine.lastIndexOf('{')) {
-            startToken = '[', endToken = ']'
+    const getValue = () => {
+      return jsonEditor && jsonEditor.getValue()
+    }
+
+    const initCodeMirror = () => {
+      nextTick(() => {
+        jsonEditor = CodeMirror.fromTextArea(textarea.value, {
+          theme: 'rubyblue',
+          mode: { name: "javascript", json: true },
+          lint: true,
+          lineNumbers: true,  // 显示行号
+          lineWrapping: true, // 文字过长时，换行
+          // showCursorWhenSelecting: true,  // 文本选中时显示光标
+          // cursorHeight: 0.85, //光标高度，默认是1
+          // matchBrackets: true, // 括号匹配
+          // smartIndent: true, // 智能缩进
+          foldGutter: true,
+          gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter', 'CodeMirror-lint-markers'],
+          extraKeys: {'Ctrl-Q': function(cm){ cm.foldCode(cm.getCursor()) }},
+          foldOptions: {
+            widget: (from, to) => {
+              var count = undefined
+
+              // Get open / close token
+              var startToken = '{', endToken = '}'
+              var prevLine = jsonEditor.getLine(from.line)
+              if (prevLine.lastIndexOf('[') > prevLine.lastIndexOf('{')) {
+                startToken = '[', endToken = ']'
+              }
+
+              // Get json content
+              var internal = jsonEditor.getRange(from, to)
+              var toParse = startToken + internal + endToken
+
+              // Get key count
+              try {
+                var parsed = JSON.parse(toParse)
+                count = Object.keys(parsed).length
+              // eslint-disable-next-line no-empty
+              } catch(e) {}
+
+              return count ? `\u21A4${count}\u21A6` : '\u2194'
+            }
           }
+        })
 
-          // Get json content
-          var internal = this.jsonEditor.getRange(from, to)
-          var toParse = startToken + internal + endToken
-
-          // Get key count
-          try {
-            var parsed = JSON.parse(toParse)
-            count = Object.keys(parsed).length
-          // eslint-disable-next-line no-empty
-          } catch(e) {}
-
-          return count ? `\u21A4${count}\u21A6` : '\u2194'
+        if (value.value) {
+          jsonEditor.setValue(JSON.stringify(value.value, null, 2))
+          jsonEditor.refresh()
         }
+        jsonEditor.on('change', cm => {
+          context.emit('update:value', cm.getValue())
+        })
+      })
+    }
+
+    onMounted(() => {
+      initCodeMirror()
+    })
+
+    watch(value.value, (val) => {
+      const editorValue = getValue()
+      if (val !== editorValue) {
+        jsonEditor.setValue(JSON.stringify(val, null, 2))
       }
     })
 
-    if (this.value) {
-      // this.jsonEditor.setValue(JSON.stringify(this.value, null, 2))
+    return {
+      textarea,
+      jsonEditor,
+      getValue
     }
-    this.jsonEditor.on('change', cm => {
-      this.$emit('update:value', cm.getValue())
-    })
-  },
-  methods: {
-    getValue() {
-      return this.jsonEditor.getValue()
-    },
   }
 }
 </script>
--- src/views/tools/json-editor.vue
+++ src/views/tools/json-editor.vue
@@ -8,18 +8,15 @@
 
 <script>
 import JsonEditor from '@/components/JsonEditor'
-import { ref } from 'vue'
+import { reactive } from 'vue'
 
-// const jsonData =
-//   '[{"items":[{"market_type":"forexdata","symbol":"XAUUSD"},{"market_type":"forexdata","symbol":"UKOIL"},{"market_type":"forexdata","symbol":"CORN"}],"name":""},{"items":[{"market_type":"forexdata","symbol":"XAUUSD"},{"market_type":"forexdata","symbol":"XAGUSD"},{"market_type":"forexdata","symbol":"AUTD"},{"market_type":"forexdata","symbol":"AGTD"}],"name":"贵金属"},{"items":[{"market_type":"forexdata","symbol":"CORN"},{"market_type":"forexdata","symbol":"WHEAT"},{"market_type":"forexdata","symbol":"SOYBEAN"},{"market_type":"forexdata","symbol":"SUGAR"}],"name":"农产品"},{"items":[{"market_type":"forexdata","symbol":"UKOIL"},{"market_type":"forexdata","symbol":"USOIL"},{"market_type":"forexdata","symbol":"NGAS"}],"name":"能源化工"}]'
-
-const jsonData = '{}'
+const jsonData = '[{"items":[{"market_type":"forexdata","symbol":"XAUUSD"},{"market_type":"forexdata","symbol":"UKOIL"},{"market_type":"forexdata","symbol":"CORN"}],"name":""},{"items":[{"market_type":"forexdata","symbol":"XAUUSD"},{"market_type":"forexdata","symbol":"XAGUSD"},{"market_type":"forexdata","symbol":"AUTD"},{"market_type":"forexdata","symbol":"AGTD"}],"name":"贵金属"},{"items":[{"market_type":"forexdata","symbol":"CORN"},{"market_type":"forexdata","symbol":"WHEAT"},{"market_type":"forexdata","symbol":"SOYBEAN"},{"market_type":"forexdata","symbol":"SUGAR"}],"name":"农产品"},{"items":[{"market_type":"forexdata","symbol":"UKOIL"},{"market_type":"forexdata","symbol":"USOIL"},{"market_type":"forexdata","symbol":"NGAS"}],"name":"能源化工"}]'
 
 export default {
   name: 'JsonEditorView',
   components: { JsonEditor },
   setup() {
-    const value = ref(JSON.parse(jsonData))
+    const value = reactive(JSON.parse(jsonData))
 
     return {
       value
